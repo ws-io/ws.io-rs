a251b2b7da0242f1323b7ae419554683e3d437b6
kiki-kanri <a470666@gmail.com>
2026-02-26 11:10:56 +0800

    chore: remove all examples files

--- examples/client.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-SCRIPTS_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-cd "${SCRIPTS_DIR}"
-
-cd client/
-cargo r --bin client --all-features "${@}"
--- examples/stress-server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-SCRIPTS_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
-cd "${SCRIPTS_DIR}"
-
-cd server/
-cargo r --bin stress --all-features "${@}"
